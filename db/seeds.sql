fe47325088e0f47847ea7077858b5e427a3de409
huntrreed <146265520+huntrreed@users.noreply.github.com>
2024-01-22 11:32:21 -0700

    created some tables

---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+INSERT INTO Department (name)
+VALUES
+
+
+INSERT INTO role (title, salary, department_id)
---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const mysql = require('mysql2');
+
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+
+// Connect to database
+const db = mysql.createConnection(
+    {
+      host: 'localhost',
+      // MySQL username,
+      user: 'root',
+      // TODO: Add MySQL password here
+      password: '',
+      database: 'employees_db'
+    },
+    console.log(`Connected to the employees_db database.`)
+  );
