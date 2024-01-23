75571ec3095df244c893bd892b480db8e9b807be
huntrreed <146265520+huntrreed@users.noreply.github.com>
2024-01-22 17:03:57 -0700

    view all employees working

--- index.js
+++ index.js
@@ -69,13 +69,36 @@ console.error('Error with Inquirer prompt:', error);
 
 //*FUNCTIONS FOR EACH MENU OPTION*//
 /*View all employee function*/
-function viewAllEmployees(){
-  db.query('SELECT * FROM employee', function (err, results) {
-    if (err) throw err;
-    console.table(results);
+function viewAllEmployees() {
+  const query = `
+    SELECT 
+      e.first_name AS 'First Name', 
+      e.last_name AS 'Last Name', 
+      r.title AS Title, 
+      d.name AS Department, 
+      r.salary AS Salary, 
+      CONCAT(m.first_name, ' ', m.last_name) AS Manager
+    FROM employee e
+    INNER JOIN role r ON e.role_id = r.id
+    INNER JOIN department d ON r.department_id = d.id
+    LEFT JOIN employee m ON e.manager_id = m.id
+    ORDER BY e.id
+  `;
+
+  db.query(query, function (err, results) {
+    if (err) {
+      console.error('Error fetching employees:', err);
+      return displayMainMenu();
+    }
+    const formattedResults = results.map(emp => ({
+      ...emp,
+      Salary: `$${new Intl.NumberFormat().format(emp.Salary)}`
+    }));
+    console.table(formattedResults);
     displayMainMenu();
   });
 }
+
 
 //*Add employee function*//
 function addEmployee(){
